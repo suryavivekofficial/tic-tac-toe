de58ab001e13f7328a6cb922a742ba146b85ee86
surya vivek <suryavivek33377@gmail.com>
2022-09-10 22:35:43 +0530

    solved weird sizing issues

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./styles/App.css";
 export default function App() {
     const [isPlayer, setIsPlayer] = useState(true);
     return (
-        <div>
+        <div className="App">
             <Board
                 isPlayer={isPlayer}
                 changePlayer={() => setIsPlayer(!isPlayer)}
